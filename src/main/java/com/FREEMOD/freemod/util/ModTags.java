9dfeae6374053331d02acde753f331c0d00d675a
arbalest3116 <kalarbalest3116@gmail.com>
2025-11-22 10:54:14 +0900

    [add]ディメンションの追加（コマンド）

---
+++ src/main/java/com/FREEMOD/freemod/util/ModTags.java
@@ -0,0 +1,38 @@
+package com.FREEMOD.freemod.util;
+
+import com.FREEMOD.freemod.main.FreeMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+
+        public static final TagKey<Block> OBLIVION_PORTAL_FRAME_BLOCKS
+                = tag("oblivion_portal_frame_blocks");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(FreeMod.MOD_ID, name));
+        }
+
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Items {
+
+
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(FreeMod.MOD_ID, name));
+        }
+
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
+        }
+    }
+}
