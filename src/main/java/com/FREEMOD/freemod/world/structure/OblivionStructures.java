ee631446af751d849730ddceff936cde01c8d90f
arbalest3116 <kalarbalest3116@gmail.com>
2025-12-28 22:08:03 +0900

    [add]Javaクラスの追加

--- src/main/java/com/FREEMOD/freemod/world/structure/OblivionStructures.java
+++ src/main/java/com/FREEMOD/freemod/world/structure/OblivionStructures.java
@@ -5,10 +5,14 @@ import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class OblivionStructures {
     public static final DeferredRegister<StructureFeature<?>> DEFERRED_REGISTRY_STRUCTURE =
             DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, FreeMod.MOD_ID);
+
+    public static final RegistryObject<StructureFeature<?>> OBLIVION_MINESHAFT =
+            DEFERRED_REGISTRY_STRUCTURE.register("oblivion_mineshaft", OblivionMineshaft::new);
 
 
     public static void register(IEventBus eventBus){
