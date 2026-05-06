1e1e9a0d36f450558c9f2d1dc918b8629f71d2f0
arbalest3116 <kalarbalest3116@gmail.com>
2026-05-06 18:03:56 +0900

    [add]MOBのテクスチャ変更テスト

--- src/main/java/com/FREEMOD/freemod/entity/renderer/FastShotSkeletonRenderer.java
+++ src/main/java/com/FREEMOD/freemod/entity/renderer/FastShotSkeletonRenderer.java
@@ -1,5 +1,6 @@
 package com.FREEMOD.freemod.entity.renderer;
 
+import com.FREEMOD.freemod.main.FreeMod;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.SkeletonRenderer;
 import net.minecraft.resources.ResourceLocation;
@@ -8,7 +9,8 @@ import net.minecraft.world.entity.monster.AbstractSkeleton;
 public class FastShotSkeletonRenderer extends SkeletonRenderer {
     // 自作テクスチャのパスを指定 (assets/freemod/textures/entity/fast_shot_skeleton.png)
     private static final ResourceLocation TEXTURE =
-            new ResourceLocation("minecraft", "textures/entity/skeleton/skeleton.png");
+            new ResourceLocation(FreeMod.MOD_ID,
+                    "textures/entity/fast_shot_skeleton.png");
 
     public FastShotSkeletonRenderer(EntityRendererProvider.Context context) {
         super(context);
